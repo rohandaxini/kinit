65c3dc7062873fb035f9e23751e6f267be8dc5bd
Rohan <rohan_daxini@yahoo.com>
2018-10-19 14:39:24 +0530

    new expect syntax instead of old should syntax in spec

--- spec/lib/kinit_spec.rb
+++ spec/lib/kinit_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 module Kinit
 	describe Kinit do
-	  it "should return true if a gem is present" do  
+	  it "should return true if a gem is present" do
 	    @gen = GemsEnforcer.new
 	    element_data = @gen.CheckIsGemPresent
-	    element_data.should_not be_nil
-	    element_data.should == true
+	    expect(element_data).not_to be_nil
+	    expect(element_data).to be true
 	  end
 	end
 end
